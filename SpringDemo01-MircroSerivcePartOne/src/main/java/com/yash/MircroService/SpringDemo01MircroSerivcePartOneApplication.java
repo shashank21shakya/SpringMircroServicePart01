886b468a9a7781b6587193057aad23361d0a9071
shashank <shashank.shakya@yash.com>
2019-07-30 15:54:34 +0530

    Initial commit

---
+++ SpringDemo01-MircroSerivcePartOne/src/main/java/com/yash/MircroService/SpringDemo01MircroSerivcePartOneApplication.java
@@ -0,0 +1,13 @@
+package com.yash.MircroService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemo01MircroSerivcePartOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemo01MircroSerivcePartOneApplication.class, args);
+	}
+
+}
---
+++ SpringDemo01-MircroSerivcePartOne/src/test/java/com/yash/MircroService/SpringDemo01MircroSerivcePartOneApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yash.MircroService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDemo01MircroSerivcePartOneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
